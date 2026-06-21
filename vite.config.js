fff066b3e00bab27a3045e6d803beab097339f6d
ftoohA <142929882+ftoohA@users.noreply.github.com>
2026-06-21 20:02:47 +0300

    v3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-})
-export default defineConfig({
   server: {
-    allowedHosts: true
+    allowedHosts: ['sb-dym6nby7bmmc.vercel.run']
   }
 })
